b10f2d3e92c82c6c52206fe147e2bfee0f083a7b
rodrigocqb <rodrigocortibarros@gmail.com>
2022-09-29 13:57:29 -0300

    GET request for categories added

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import express from "express";
 import cors from "cors";
+import categoryRouter from "./routers/categoryRouter.js";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.use(categoryRouter);
 
 export default app;
