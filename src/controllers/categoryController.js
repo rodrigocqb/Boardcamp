3e112eed61c2ab6c462533fa57c2251dada80696
rodrigocqb <rodrigocortibarros@gmail.com>
2022-09-29 15:01:27 -0300

    POST request for categories added

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -1,4 +1,5 @@
 import connection from "../database/database.js";
+import { categorySchema } from "../schemas/categorySchema.js";
 
 async function getCategories(req, res) {
   try {
@@ -9,4 +10,28 @@ async function getCategories(req, res) {
   }
 }
 
-export { getCategories };
+async function createCategory(req, res) {
+  const { name } = req.body;
+  const validation = categorySchema.validate(req.body);
+  if (validation.error) {
+    const error = validation.error.details[0].message;
+    return res.status(400).send(error);
+  }
+  try {
+    const exists = await connection.query(
+      "SELECT * FROM categories WHERE name = $1;",
+      [name]
+    );
+    if (exists.rows[0]) {
+      return res.status(409).send({ error: "Category already exists" });
+    }
+    await connection.query("INSERT INTO categories (name) VALUES ($1);", [
+      name,
+    ]);
+    res.sendStatus(201);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
+
+export { getCategories, createCategory };
--- src/routers/categoryRouter.js
+++ src/routers/categoryRouter.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { getCategories } from "../controllers/categoryController.js";
+import {
+  createCategory,
+  getCategories,
+} from "../controllers/categoryController.js";
 
 const router = express.Router();
 
 router.get("/categories", getCategories);
+router.post("/categories", createCategory);
 
 export default router;
