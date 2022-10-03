f5d5ac7c8b02e4e66533e8ca6bcea533fb8316d5
rodrigocqb <rodrigocortibarros@gmail.com>
2022-10-02 21:09:54 -0300

    Added limit, offset and order by queries to categories route

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -2,8 +2,30 @@ import connection from "../database/database.js";
 import { categorySchema } from "../schemas/categorySchema.js";
 
 async function getCategories(req, res) {
+  const { limit } = req.query;
+  let { offset, order, desc } = req.query;
+  if (!offset) {
+    offset = 0;
+  }
+  if (!order || (order !== "name" && order !== "id")) {
+    order = "id";
+  }
+  if (desc === "true") {
+    desc = "DESC";
+  } else {
+    desc = "ASC";
+  }
   try {
-    const categories = await connection.query("SELECT * FROM categories;");
+    if (limit) {
+      const categories = await connection.query(
+        `SELECT * FROM categories ORDER BY ${order} ${desc} LIMIT $1 OFFSET $2;`,
+        [limit, offset]
+      );
+      return res.status(200).send(categories.rows);
+    }
+    const categories = await connection.query(
+      `SELECT * FROM categories ORDER BY ${order} ${desc};`
+    );
     res.status(200).send(categories.rows);
   } catch (error) {
     res.status(500).send(error.message);
