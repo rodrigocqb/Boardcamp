9473af99a3708294e26b4369100aea9549b74371
rodrigocqb <rodrigocortibarros@gmail.com>
2022-10-08 18:36:24 -0300

    Added .env.example file and scripts to package.json file

--- src/server.js
+++ src/server.js
@@ -1,3 +1,7 @@
 import app from "./app.js";
+import dotenv from "dotenv";
+dotenv.config();
 
-app.listen(4000, () => console.log("Server listening on port 4000"));
+app.listen(process.env.PORT, () =>
+  console.log(`Server listening on port ${process.env.PORT}`)
+);
